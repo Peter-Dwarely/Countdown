f6003bf10eee25c36419a6af0a207a968a585860
PPPPPPPETER <91359436+Peter-Dwarely@users.noreply.github.com>
2024-09-11 17:26:48 +0800

    Update Package.py

--- Package.py
+++ Package.py
@@ -4,6 +4,7 @@ PyInstaller.__main__.run([
     '--name=CountDown18_30',
     '--onefile',
     '--noconsole',
-    '--upx-dir=C:\\Users\\HKPL711918\\Downloads\\upx-4.2.4-win64 (2)\\upx-4.2.4-win64',  # 修正路径
+    '--upx-dir=C:\\Users\\HKPL711918\\Downloads\\upx-4.2.4-win64 (2)\\upx-4.2.4-win64',  # 添加自己的upx的修正路径
     'main1.py'
+    
 ])
